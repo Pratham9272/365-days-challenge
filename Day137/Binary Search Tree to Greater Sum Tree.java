bc973ddc9bbf6fe7d0cd9fb0262034cc2d358824
Pratham9272 <prathameshpatil9272@gmail.com>
2025-05-17 21:30:50 +0530

    1.Binary Search Tree to Greater Sum Tree

---
+++ Day137/Binary Search Tree to Greater Sum Tree.java
@@ -0,0 +1,18 @@
+class Solution {
+    private int acc=0;
+    public TreeNode bstToGst(TreeNode root) {
+        reverseInorder(root);
+        return root;
+    }
+
+    private void reverseInorder(TreeNode node){
+        if(node==null) return;
+
+        reverseInorder(node.right);
+        
+        acc+=node.val;
+        node.val=acc;
+
+        reverseInorder(node.left);
+    }
+}
